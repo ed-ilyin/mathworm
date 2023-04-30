cdec951420ca7a7a145b6bfb37090d5a0d19c3b9
Ed Ilyin <Ed.Il@live.com>
2023-04-30 18:03:58 +0300

    now random respects size of view

--- src/routes/tuc/game/+page.svelte
+++ src/routes/tuc/game/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import Worm from './Worm.svelte';
 	import Snake from './Snake.svelte';
-	let seeds = ['2^5', '3^3', '5*3', '45+45+110', '5^4', '2^4', '10^4', '3^0', '1^-1', '20^3'];
+	let exercises = ['2^5', '3^3', '5*3', '45+45+110', '5^4', '2^4', '10^4', '3^0', '1^-1', '20^3'];
+	let seeds: any[] = [];
+	let size = 56;
+	onMount(
+		() =>
+			(seeds = exercises.map((exercise) => {
+				return {
+					exercise: exercise,
+					x: Math.random() * (window.innerWidth - size),
+					y: Math.random() * (window.innerHeight - size)
+				};
+			}))
+	);
 </script>
 
 <svelte:head>
@@ -11,8 +24,8 @@
 
 <a href="/tuc"><button>back ⇇</button></a>
 {#each seeds as seed}
-	<div class="seed" style="transform: translate({Math.random() * 500}px,{Math.random() * 500}px">
-		<h4>{seed}</h4>
+	<div class="seed" style="width:{size}px;eight:{size}px;left:{seed.x}px;top:{seed.y}px">
+		<h4>{seed.exercise}</h4>
 	</div>
 {/each}
 <Worm kp={0.0001} ki={0.000001} kd={0.07}><Snake /></Worm>
@@ -26,9 +39,8 @@
 
 <style>
 	.seed {
+		position: absolute;
 		background: rebeccapurple;
-		width: 3.49rem;
-		height: 3.5rem;
 		z-index: -1;
 	}
 </style>
