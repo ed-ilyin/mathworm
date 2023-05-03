cbfd3695fa03dd91717df7523f35e21d1952caa7
Ed Ilyin <Ed.Il@live.com>
2023-05-03 16:57:01 +0300

    moves moon to frag file

--- src/routes/3d/moon/+page.svelte
+++ src/routes/3d/moon/+page.svelte
@@ -9,53 +9,16 @@
     in vec2 a_position;
     void main() { gl_Position = vec4(a_position, 0., 1.); }`;
 
-	const fragmentShaderSource = `#version 300 es
-precision highp float;
-uniform vec2 u_resolution;
-uniform float u_time;
-out vec4 outColor;
-
-vec2 sphIntersect(vec3 ro,vec3 rd,float ra){
-  float b=dot(ro,rd);
-  float c=dot(ro,ro)-ra*ra;
-  float h=b*b-c;
-  if(h<0.){
-    return vec2(-1.);
-  }
-  h=sqrt(h);
-  return vec2(-b-h,-b+h);
-}
-
-vec3 castRay(vec3 ro,vec3 rd){
-  vec2 t=sphIntersect(ro,rd,1.);
-  if(t.x<0.){
-    return vec3(rd.y,rd.z,.5);
-  }
-  vec3 pos=ro+rd*t.x;
-  vec3 nor=normalize(pos);
-  vec3 light=normalize(vec3(cos(u_time),sin(u_time),sin(u_time)));
-  // vec3 light=normalize(vec3(-4));
-  float diffuse=dot(light,nor);
-  // float specular=pow(max(dot(reflect(rd,nor),light),0.),64.);
-  vec3 col=vec3(diffuse);
-  return col;
-}
-
-void main(){
-  vec2 uv=gl_FragCoord.xy/u_resolution-.5;
-  float aspectRatio=u_resolution.x/u_resolution.y;
-  if(aspectRatio<1.){uv.y/=aspectRatio;}else{uv.x*=aspectRatio;}
-  vec3 ro=vec3(-3.,0.,0.);
-  vec3 rd=normalize(vec3(1.,uv.x,uv.y));
-  vec3 col=castRay(ro,rd);
-  outColor=vec4(col,1.);
-}`;
-
-	onMount(() => {
+	function updateCanvasSize() {
 		canvas.width = window.innerWidth * window.devicePixelRatio;
 		canvas.height = window.innerHeight * window.devicePixelRatio;
-		gl = canvas.getContext('webgl2') as WebGL2RenderingContext;
 		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+	}
+
+	onMount(async () => {
+		gl = canvas.getContext('webgl2') as WebGL2RenderingContext;
+		updateCanvasSize();
+		window.addEventListener('resize', updateCanvasSize);
 
 		const vertexShader = gl.createShader(gl.VERTEX_SHADER) as WebGLShader;
 		gl.shaderSource(vertexShader, vertexShaderSource);
@@ -65,6 +28,7 @@ void main(){
 			return;
 		}
 		const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER) as WebGLShader;
+		let fragmentShaderSource = await fetch('moon.frag').then((res) => res.text());
 		gl.shaderSource(fragmentShader, fragmentShaderSource);
 		gl.compileShader(fragmentShader);
 		if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
@@ -90,11 +54,11 @@ void main(){
 
 		const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
 		gl.useProgram(program);
-		gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
 		const timeUniformLocation = gl.getUniformLocation(program, 'u_time');
 
 		function render(time: number) {
+			gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 			gl.uniform1f(timeUniformLocation, time / 1000);
 			gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 			requestId = requestAnimationFrame(render);
@@ -102,6 +66,7 @@ void main(){
 		requestId = requestAnimationFrame(render);
 		return () => {
 			cancelAnimationFrame(requestId);
+			window.removeEventListener('resize', updateCanvasSize);
 			gl.deleteProgram(program);
 			gl.deleteShader(fragmentShader);
 			gl.deleteShader(vertexShader);
